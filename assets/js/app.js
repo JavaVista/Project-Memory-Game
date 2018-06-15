1d37601c1e31fee1caf065830040c5c4cd70b3ac
Javi Carrion <suavejavi@google.com>
2018-06-15 15:15:28 -0400

    feat: Add reveal and shuffle deck of cards functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -69,3 +69,14 @@ deal();
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+const allCards = document.querySelectorAll('.card');
+console.log(allCards);
+
+// Reveal card and icon symbol when click
+deckField.addEventListener('click', e => {
+    const target = e.target;
+    if (target.classList.contains('card')) {
+        target.classList.toggle('open');
+        target.classList.toggle('show');
+    }
+})
